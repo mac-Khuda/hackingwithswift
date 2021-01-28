22bd6e86e9a55fffa117594d6f8d372591b76dc6
Volodymyr Khuda <volodymyrkhuda@Volodymyrs-MacBook-Pro.local>
2021-01-28 12:28:55 +0200

    Day 33, Tab Bar bug fixed

--- 100 days of Swift/Project 7/Project 7/ViewController.swift
+++ 100 days of Swift/Project 7/Project 7/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UITableViewController {
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
-            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
         if let url = URL(string: urlString) {
