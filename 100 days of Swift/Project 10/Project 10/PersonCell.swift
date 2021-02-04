2e9d6cf3bd850b042358ac23a50f2bba13b2d4a4
Volodymyr Khuda <volodymyrkhuda@Volodymyrs-MacBook-Pro.local>
2021-02-04 23:50:21 +0200

    Day 42 done

---
+++ 100 days of Swift/Project 10/Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Volodymyr Khuda on 04.02.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var name: UIView!
+    
+}
