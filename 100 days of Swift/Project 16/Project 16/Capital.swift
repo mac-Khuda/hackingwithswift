6bda7e18a4ad184f998d059bbc2c92dda2295e48
Volodymyr Khuda <volodymyrkhuda@Volodymyrs-MacBook-Pro.local>
2021-02-25 23:42:36 +0200

    Day 60 done

---
+++ 100 days of Swift/Project 16/Project 16/Capital.swift
@@ -0,0 +1,20 @@
+//
+//  Capital.swift
+//  Project 16
+//
+//  Created by Volodymyr Khuda on 25.02.2021.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
